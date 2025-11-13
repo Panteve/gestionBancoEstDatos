ff6be861a3ba47a523f5a8b029a289a18f803535
Panteve <diegorojer@gmail.com>
2025-11-13 15:20:44 -0500

    Inicializacion del repositorio

---
+++ GestionBanco/src/main/java/com/mycompany/gestionbanco/GestionBanco.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.gestionbanco;
+
+/**
+ *
+ * @author DIEGO
+ */
+public class GestionBanco {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}

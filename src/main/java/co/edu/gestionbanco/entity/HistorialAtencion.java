0ceb0c2fdd875c3c78d873d9b506f72c3403735c
Panteve <diegorojer@gmail.com>
2025-11-20 17:25:59 -0500

    Clase de historial de atencion creada

---
+++ src/main/java/co/edu/gestionbanco/entity/HistorialAtencion.java
@@ -0,0 +1,68 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package co.edu.gestionbanco.entity;
+
+/**
+ *
+ * @author DIEGO
+ */
+public class HistorialAtencion {
+    private int id_empleado;
+    private int id_turno;
+    private String horaLlamado;
+    private String horaFinalizacion;
+
+    public HistorialAtencion(int id_empleado, int id_turno, String horaLlamado, String horaFinalizacion) {
+        this.id_empleado = id_empleado;
+        this.id_turno = id_turno;
+        this.horaLlamado = horaLlamado;
+        this.horaFinalizacion = horaFinalizacion;
+    }
+
+    public int getId_empleado() {
+        return id_empleado;
+    }
+
+    public void setId_empleado(int id_empleado) {
+        this.id_empleado = id_empleado;
+    }
+
+    public int getId_turno() {
+        return id_turno;
+    }
+
+    public void setId_turno(int id_turno) {
+        this.id_turno = id_turno;
+    }
+
+    public String getHoraLlamado() {
+        return horaLlamado;
+    }
+
+    public void setHoraLlamado(String horaLlamado) {
+        this.horaLlamado = horaLlamado;
+    }
+
+    public String getHoraFinalizacion() {
+        return horaFinalizacion;
+    }
+
+    public void setHoraFinalizacion(String horaFinalizacion) {
+        this.horaFinalizacion = horaFinalizacion;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("HistorialAtencion{");
+        sb.append("id_empleado=").append(id_empleado);
+        sb.append(", id_turno=").append(id_turno);
+        sb.append(", horaLlamado=").append(horaLlamado);
+        sb.append(", horaFinalizacion=").append(horaFinalizacion);
+        sb.append('}');
+        return sb.toString();
+    }
+    
+}

0067a018426d85d657a8ed1429b906d5e2b1b07f
Panteve <diegorojer@gmail.com>
2025-11-20 17:09:34 -0500

    Clase del dticket creada con sus respectivos atributos

--- src/main/java/co/edu/gestionbanco/entity/Ticket.java
+++ src/main/java/co/edu/gestionbanco/entity/Ticket.java
@@ -1,13 +1,77 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package co.edu.gestionbanco.entity;
 
-/**
- *
- * @author DIEGO
- */
 public class Ticket {
-    
+    private String codigo;
+    private String fecha;
+    private String hora;
+    private int servicio_id;
+    private int cliente_id;
+    private String estado;
+    private int prioridad_id;
+
+    public Ticket(String codigo, String fecha, String hora, int servicio_id, int cliente_id, String estado, int prioridad_id) {
+        this.codigo = codigo;
+        this.fecha = fecha;
+        this.hora = hora;
+        this.servicio_id = servicio_id;
+        this.cliente_id = cliente_id;
+        this.estado = estado;
+        this.prioridad_id = prioridad_id;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(String fecha) {
+        this.fecha = fecha;
+    }
+
+    public String getHora() {
+        return hora;
+    }
+
+    public void setHora(String hora) {
+        this.hora = hora;
+    }
+
+    public int getServicio_id() {
+        return servicio_id;
+    }
+
+    public void setServicio_id(int servicio_id) {
+        this.servicio_id = servicio_id;
+    }
+
+    public int getCliente_id() {
+        return cliente_id;
+    }
+
+    public void setCliente_id(int cliente_id) {
+        this.cliente_id = cliente_id;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    public int getPrioridad_id() {
+        return prioridad_id;
+    }
+
+    public void setPrioridad_id(int prioridad_id) {
+        this.prioridad_id = prioridad_id;
+    }
 }

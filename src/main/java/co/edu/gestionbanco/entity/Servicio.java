a5e5c83e6cf275e3edd194c2df4cd86f3b15cb80
Panteve <diegorojer@gmail.com>
2025-11-27 14:17:40 -0500

    Termino todas las funciones CRUD de servicios

--- src/main/java/co/edu/gestionbanco/entity/Servicio.java
+++ src/main/java/co/edu/gestionbanco/entity/Servicio.java
@@ -9,12 +9,25 @@ package co.edu.gestionbanco.entity;
  * @author DIEGO
  */
 public class Servicio {
+    private int id_servicio;
     private String nombre;
     private String descripcion;
 
-    public Servicio(String nombre, String descripcion) {
+    public Servicio(int id_servicio, String nombre, String descripcion) {
+        this.id_servicio = id_servicio;
         this.nombre = nombre;
         this.descripcion = descripcion;
+    }
+
+    public Servicio() {
+    }
+    
+    public int getId_servicio() {
+        return id_servicio;
+    }
+
+    public void setId_servicio(int id_servicio) {
+        this.id_servicio = id_servicio;
     }
 
     public String getNombre() {
@@ -37,10 +50,13 @@ public class Servicio {
     public String toString() {
         StringBuilder sb = new StringBuilder();
         sb.append("Servicio{");
-        sb.append("nombre=").append(nombre);
+        sb.append("id_servicio=").append(id_servicio);
+        sb.append(", nombre=").append(nombre);
         sb.append(", descripcion=").append(descripcion);
         sb.append('}');
         return sb.toString();
     }
+
+
     
 }
---
+++ src/main/java/co/edu/gestionbanco/repository/ServicioRepository.java
@@ -0,0 +1,96 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package co.edu.gestionbanco.repository;
+
+import co.edu.gestionbanco.entity.Prioridad;
+import co.edu.gestionbanco.entity.Servicio;
+import co.edu.gestionbanco.entity.Turno;
+import co.edu.gestionbanco.util.ConexionBD;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author DIEGO
+ */
+public class ServicioRepository {
+    //Atributos 
+    private ConexionBD conexionBD;//Objeto
+    private PreparedStatement preStm;
+
+    //Constructor
+    public ServicioRepository() {
+        this.conexionBD = new ConexionBD();
+        this.preStm = null;
+    }
+    
+    public Servicio getServicio(int id_turno) {
+        Connection con = conexionBD.getConectionDB();
+        String sqlQuery = "SELECT * FROM servicios WHERE id_servicio = ?"; //Agregar order by para traer en orden la lista por prioridad 
+        Servicio servicio = new Servicio();
+        try {
+            this.preStm = con.prepareStatement(sqlQuery);
+            this.preStm.setInt(1, id_turno);
+            ResultSet resultSet = this.preStm.executeQuery();
+            if (resultSet.next()) {
+                servicio = new Servicio(
+                        resultSet.getInt("id_servicio"),
+                        resultSet.getString("nombre"),
+                        resultSet.getString("descripcion")
+                );
+            }
+        } catch (SQLException e) {
+            System.out.println("Error en la sentencia:" + e.getMessage());
+        } catch (Exception e) {
+            System.out.println("error:" + e.getMessage());
+        } finally {
+            if ((con != null) && (this.preStm != null)) {
+                try {
+                    con.close();
+                    this.preStm.close();
+                } catch (SQLException ex) {
+                    System.out.println("error" + ex.getMessage());
+                }
+            }
+        }
+        return servicio;
+    }
+    
+    public  List<Servicio> getAllTurnos() {
+        Connection con = conexionBD.getConectionDB();
+        String sqlQuery = "SELECT * FROM turnos WHERE estado = 'En espera'"; //Agregar order by para traer en orden la lista por prioridad 
+        List<Servicio> servicioList = new ArrayList<>();
+        try {
+            this.preStm = con.prepareStatement(sqlQuery);
+            //Los datos de la tabla se guardan en el resultSet
+            ResultSet resultSet = this.preStm.executeQuery();
+            while (resultSet.next()) {
+                servicioList.add(new Servicio(
+                        resultSet.getInt("id_servicio"),
+                        resultSet.getString("nombre"),
+                        resultSet.getString("descripcion")
+                ));
+            }
+        } catch (SQLException e) {
+            System.out.println("Error en la sentencia:" + e.getMessage());
+        } catch (Exception e) {
+            System.out.println("error:" + e.getMessage());
+        } finally {
+            if ((con != null) && (this.preStm != null)) {
+                try {
+                    con.close();
+                    this.preStm.close();
+                } catch (SQLException ex) {
+                    System.out.println("error" + ex.getMessage());
+                }
+            }
+        }
+        return servicioList;
+    }
+}
